5c95695874eb40a919fcf57c97a1abccfff19333
Alexander Graebe <agraebe@users.noreply.github.com>
2021-03-22 16:32:47 -0700

    feat: add initial tests

--- tests/swag-nft_test.ts
+++ tests/swag-nft_test.ts
@@ -3,24 +3,15 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure initial token distribution works",
     async fn(chain: Chain, accounts: Array<Account>) {
         let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+            Tx.contractCall("swag-nft", "get-last-token-id", [], accounts[0].address),
+            Tx.contractCall("swag-nft", "get-owner", [types.uint(1)], accounts[0].address),
         ]);
-        assertEquals(block.receipts.length, 0);
+        assertEquals(block.receipts.length, 2);
+        assertEquals(block.receipts[0].result, "(ok u3)");
+        assertEquals(block.receipts[1].result, "(ok (some ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH))");
         assertEquals(block.height, 2);
-
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
     },
 });
