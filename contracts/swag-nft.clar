1a646ca8a6bf75c063b0edfa9483a5bd05f51301
Alexander Graebe <agraebe@users.noreply.github.com>
2021-04-05 14:16:03 -0700

    feat: remove unwanted code, fix some issues

--- contracts/swag-nft.clar
+++ contracts/swag-nft.clar
@@ -1,18 +1,15 @@
 (impl-trait 'ST000000000000000000002AMW42H.nft-trait.nft-trait)
-(define-non-fungible-token swag uint)
+(define-non-fungible-token swag-100 uint)
+
+;; Limited to first 100 users
+(define-constant max-tokens u100)
 
 ;; Error handling
+(define-constant nft-max-reached (err u403)) ;; no more tokens availabale
 (define-constant nft-not-owned-err (err u401)) ;; unauthorized
-(define-constant not-approved-spender-err (err u403)) ;; forbidden
 (define-constant nft-not-found-err (err u404)) ;; not found
 (define-constant sender-equals-recipient-err (err u405)) ;; method not allowed
 (define-constant nft-exists-err (err u409)) ;; conflict
-
-(define-map err-strings (response uint uint) (string-ascii 32))
-(map-insert err-strings nft-not-owned-err "nft-not-owned")
-(map-insert err-strings not-approved-spender-err "not-approaved-spender")
-(map-insert err-strings nft-not-found-err "nft-not-found")
-(map-insert err-strings nft-exists-err "nft-exists")
 
 (define-private (nft-transfer-err (code uint))
   (if (is-eq u1 code)
@@ -28,9 +25,6 @@
     nft-exists-err
     (err code)))
 
-(define-read-only (get-errstr (code uint))
-  (unwrap! (map-get? err-strings (err code)) "unknown-error"))
-
 ;; Storage
 (define-map tokens-count principal uint)
 (define-data-var last-id uint u0)
@@ -38,17 +32,24 @@
 ;; Transfers tokens to a specified principal.
 (define-public (transfer (token-id uint) (sender principal) (recipient principal))
   (if (and
-        (is-eq tx-sender (unwrap! (nft-get-owner? swag token-id) nft-not-found-err))
-        (is-eq tx-sender sender)
-        (not (is-eq recipient sender)))
-       (match (nft-transfer? swag token-id sender recipient)
+        (is-eq tx-sender sender))
+      (match (nft-transfer? swag-100 token-id sender recipient)
         success (ok success)
         error (nft-transfer-err error))
       nft-not-owned-err))
 
+;; Claim a new swag-100 token.
+(define-public (claim-swag)
+  (if 
+    (and 
+      (< (var-get last-id) max-tokens)
+      (is-eq (balance-of tx-sender) u0))
+        (ok (mint tx-sender))
+        (err nft-max-reached)))
+
 ;; Gets the owner of the specified token ID.
 (define-read-only (get-owner (token-id uint))
-  (ok (nft-get-owner? swag token-id)))
+  (ok (nft-get-owner? swag-100 token-id)))
 
 ;; Gets the owner of the specified token ID.
 (define-read-only (get-last-token-id)
@@ -70,7 +71,7 @@
 ;; Internal - Register token
 (define-private (mint (new-owner principal))
     (let ((current-balance (balance-of new-owner)) (next-id (+ u1 (var-get last-id))))
-      (match (nft-mint? swag next-id new-owner)
+      (match (nft-mint? swag-100 next-id new-owner)
         success
           (begin
             (map-set tokens-count
@@ -79,9 +80,3 @@
             (var-set last-id next-id)
             (ok success))
         error (nft-mint-err error))))
-
-;; Initialize the contract
-(begin
-  (try! (mint 'ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH))
-  (try! (mint 'ST1P4WHXG566QVTAGXPFWPSMXKH0HEBY1S3VZ7PCA))
-  (try! (mint 'ST2EGN8HCFSKEH0XKKG3FYQRE4RG0NYMD7NFZ8PP5)))
--- tests/swag-nft_test.ts
+++ tests/swag-nft_test.ts
@@ -3,15 +3,34 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Initial token distribution",
+    name: "Token distribution",
     async fn(chain: Chain, accounts: Array<Account>) {
         let block = chain.mineBlock([
+            // start at 0
             Tx.contractCall("swag-nft", "get-last-token-id", [], accounts[0].address),
+            // mint
+            Tx.contractCall("swag-nft", "claim-swag", [], accounts[0].address),
+            // mint
+            Tx.contractCall("swag-nft", "claim-swag", [], accounts[1].address),
+            // mint
+            Tx.contractCall("swag-nft", "claim-swag", [], accounts[2].address),
+            // double mint
+            Tx.contractCall("swag-nft", "claim-swag", [], accounts[0].address),
+            // check balance 
             Tx.contractCall("swag-nft", "get-owner", [types.uint(1)], accounts[0].address),
+            Tx.contractCall("swag-nft", "get-owner", [types.uint(5)], accounts[6].address),
+            // check last id
+            Tx.contractCall("swag-nft", "get-last-token-id", [], accounts[0].address),
         ]);
-        assertEquals(block.receipts.length, 2);
-        assertEquals(block.receipts[0].result, "(ok u3)");
-        assertEquals(block.receipts[1].result, "(ok (some ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH))");
+        assertEquals(block.receipts.length, 8);
+        assertEquals(block.receipts[0].result, "(ok u0)");
+        assertEquals(block.receipts[1].result, "(ok (ok true))");
+        assertEquals(block.receipts[2].result, "(ok (ok true))");
+        assertEquals(block.receipts[3].result, "(ok (ok true))");
+        assertEquals(block.receipts[4].result, "(err (err u403))");
+        assertEquals(block.receipts[5].result, "(ok (some ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH))");
+        assertEquals(block.receipts[6].result, "(ok none)");
+        assertEquals(block.receipts[7].result, "(ok u3)");
     },
 });
 
@@ -34,16 +53,46 @@ Clarinet.test({
     name: "Transfer between accounts",
     async fn(chain: Chain, accounts: Array<Account>) {
         let block = chain.mineBlock([
-            Tx.contractCall("swag-nft", "get-token-uri", [types.uint(1)], accounts[0].address),
+            // mint
+            Tx.contractCall("swag-nft", "claim-swag", [], accounts[0].address),
+            // wrong sender
+            Tx.contractCall("swag-nft", "transfer", [types.uint(1), types.principal('ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH'), types.principal('ST3AA33M8SS15A30ETXE134ZXD8TNEDHT8Q955G40')], accounts[4].address),
+            // account does not own token
+            Tx.contractCall("swag-nft", "transfer", [types.uint(2), types.principal('ST398MYTA19HSZFCFXWSB9VYAXYJXED4Z9QFKWG4W'), types.principal('ST3AA33M8SS15A30ETXE134ZXD8TNEDHT8Q955G40')], accounts[0].address),
+            // sender equals recipient
+            Tx.contractCall("swag-nft", "transfer", [types.uint(1), types.principal('ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH'), types.principal('ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH')], accounts[0].address),
+            // success
+            Tx.contractCall("swag-nft", "transfer", [types.uint(1), types.principal('ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH'), types.principal('ST3AA33M8SS15A30ETXE134ZXD8TNEDHT8Q955G40')], accounts[0].address),
+            // verify owner change
+            Tx.contractCall("swag-nft", "get-owner", [types.uint(1)], accounts[6].address),
+            // token id does not exist
+            Tx.contractCall("swag-nft", "transfer", [types.uint(5), types.principal('ST238B5WSC8B8XETWDXMH7HZC2MJ2RNTYY15YY7SH'), types.principal('ST3AA33M8SS15A30ETXE134ZXD8TNEDHT8Q955G40')], accounts[0].address),
         ]);
-        assertEquals(block.receipts.length, 3);
-        assertEquals(block.receipts[0].result, '(ok (some "https://docs.blockstack.org"))');
+        assertEquals(block.receipts.length, 7);
+        assertEquals(block.receipts[1].result, "(err u401)");
+        assertEquals(block.receipts[2].result, "(err u401)");
+        assertEquals(block.receipts[3].result, '(err u405)');
+        assertEquals(block.receipts[4].result, '(ok true)');
+        assertEquals(block.receipts[5].result, '(ok (some ST3AA33M8SS15A30ETXE134ZXD8TNEDHT8Q955G40))');
+        assertEquals(block.receipts[6].result, '(err u404)');
     },
 });
 
-/*
-missing:
-- transfer
-- mint
-- balance-of
-*/
+Clarinet.test({
+    name: "No more than 100 tokens",
+    async fn(chain: Chain, accounts: Array<Account>) {
+        let blocks = [];
+
+        for (let i = 0; i < 100; i++) {
+            blocks.push(
+                // mint
+                Tx.contractCall("swag-nft", "claim-swag", [], accounts[i].address),
+            );
+        }
+
+        let block = chain.mineBlock(blocks);
+
+        assertEquals(block.receipts.length, 101);
+        assertEquals(block.receipts[100].result, "(err (err u403))");
+    },
+});
